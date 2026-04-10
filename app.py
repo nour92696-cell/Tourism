b5874bd1071d47019fb8db98144a7bcef7b3aed9
nour92696-cell <nour92696@gmail.com>
2026-04-10 13:56:10 +0100

    Mise en place de l'architecture du projet

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask 
-import openai
-app= Flask(__tourism__)
-@app.route('/')
-def home():
-        return"Bienvenue dand mon projet de recherche IA !"
-        if__tourism__=='__main__':
-            app.run(debug=True)
